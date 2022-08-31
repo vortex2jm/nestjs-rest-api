1c1d9484e61d361f991f521138e70f8a8578d1bd
vortex2jm <joaopaulomoura5328@gmail.com>
2022-08-31 20:07:12 -0300

    :sparkles: Adding database connection

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
+import { Student } from './entities/student.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Student])],
   controllers: [StudentsController],
   providers: [StudentsService]
 })
