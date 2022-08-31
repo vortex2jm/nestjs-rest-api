6242d0d504ab13e136b71a2f365a74bbf91b2e3b
vortex2jm <joaopaulomoura5328@gmail.com>
2022-08-31 20:05:49 -0300

    :sparkles: Adding database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Student } from './students/entities/student.entity';
 
 @Module({
-  imports: [StudentsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: "postgres",
+    host: "db",
+    port: 5432,
+    username: "postgres",
+    password: "root",
+    database: "store",
+    synchronize: true,
+    logging: false,
+    entities: [Student]
+  }) , StudentsModule],
+
   controllers: [AppController],
   providers: [AppService],
 })
