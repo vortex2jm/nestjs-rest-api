fcc26cf6077ed6c8b5ea8ff1f902800f009d8833
vortex2jm <joaopaulomoura5328@gmail.com>
2022-08-31 20:07:42 -0300

    :sparkles: Creating entity

--- src/students/entities/student.entity.ts
+++ src/students/entities/student.entity.ts
@@ -1,1 +1,17 @@
-export class Student {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: "students"})
+export class Student {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column()
+    name: string;
+
+    @Column({type: "decimal", precision: 2, nullable: true})
+    media: number;
+
+    @Column()
+    registration: number;
+}
