f48977c5287f7180caf3e4b6fe0b44f5ff34b60b
vortex2jm <joaopaulomoura5328@gmail.com>
2022-08-31 20:06:34 -0300

    :sparkles: Adding business logic

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,26 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityNotFoundError, Repository } from 'typeorm';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { Student } from './entities/student.entity';
 
 @Injectable()
 export class StudentsService {
+
+  constructor(
+    @InjectRepository(Student)
+    private studentRepo: Repository<Student>
+  ){}
+
   create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
+    const student = this.studentRepo.create(createStudentDto);
+    return this.studentRepo.save(student)
   }
 
   findAll() {
-    return `This action returns all students`;
+    return this.studentRepo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} student`;
+    return this.studentRepo.findOneBy({id: id});
   }
 
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
+  async update(id: number, updateStudentDto: UpdateStudentDto) {
+    
+    const updateRes = await this.studentRepo.update(id, updateStudentDto)
+    if(!updateRes.affected){
+      throw new EntityNotFoundError(Student, id)
+    }
+    return this.studentRepo.findOneBy({id: id});
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} student`;
+  async remove(id: number) {
+    const deleteRes = await this.studentRepo.delete(id);
+    if(!deleteRes.affected){
+      throw new EntityNotFoundError(Student, id)
+    }
   }
 }
