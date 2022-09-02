ddd880aa93dfe0e7c1212a6e27e6d9e61302bfc1
vortex2jm <joaopaulomoura5328@gmail.com>
2022-09-02 14:59:10 -0300

    :sparkles: Adding DTOs with validators

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,1 +1,17 @@
-export class CreateStudentDto {}
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from "class-validator"
+
+export class CreateStudentDto {
+    
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(255)
+    name: string
+
+    @IsNotEmpty()
+    @IsNumber()
+    media: number
+
+    @IsNotEmpty()
+    @IsNumber()
+    registration: number
+}
--- src/students/dto/update-student.dto.ts
+++ src/students/dto/update-student.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 import { CreateStudentDto } from './create-student.dto';
 
-export class UpdateStudentDto extends PartialType(CreateStudentDto) {}
+export class UpdateStudentDto extends PartialType(CreateStudentDto) {
+
+    @IsNotEmpty()
+    @IsNumber()
+    media: number;
+}
